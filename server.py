f685ffa85be0e7d9e6a2c3461c70e22a274fd9a3
giuspo1980 <61871243+giuspo1980@users.noreply.github.com>
2020-03-06 14:45:36 +0100

    Delete server.py

--- server.py
+++
@@ -1,5 +0,0 @@
-export_file_url="https://www.dropbox.com/s/cl93f7zylq8g46d/export.pkl?dl=0"
-export_file_name="export.pkl"
-
-classes =['coltelli', 'cucchiai', 'forchette']
-path = Path(__file__).parent
